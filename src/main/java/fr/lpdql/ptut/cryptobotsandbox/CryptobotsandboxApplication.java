dd4e88a34692b4f763fdc71d5d8b9efe506861d2
Florian Leca <florianleca@hotmail.fr>
2023-03-15 09:33:09 +0100

    first commit

---
+++ src/main/java/fr/lpdql/ptut/cryptobotsandbox/CryptobotsandboxApplication.java
@@ -0,0 +1,13 @@
+package fr.lpdql.ptut.cryptobotsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptobotsandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptobotsandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lpdql/ptut/cryptobotsandbox/CryptobotsandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lpdql.ptut.cryptobotsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptobotsandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
